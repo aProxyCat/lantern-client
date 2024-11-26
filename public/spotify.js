91ee558a845b3cefafa993f5d12ee2178b10594a
imTear <136250472+aProxyCat@users.noreply.github.com>
2024-11-25 19:54:21 -0700

    Update spotify.js

--- public/spotify.js
+++ public/spotify.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Set a predefined URL
-    let predefinedUrl = "https://spotify-react-web-client.onrender.com/"; // Change this to any URL you want to automatically use
+    let predefinedUrl = "https://spotify-react-web-client.onrender.com/?code=AQDNKCPJzv_7mAXtY4CyRW_Z834r86wWiAg1zgASeFkqzAwpuJ88yB1H-54AQj1TLR_lL2SDCS35jn3_6ieHgmin2f-UkYN5N42B7rDWGfoenaRKpH8Dd62GgFjpadgWRGw1Qz2m3rJjsaFXAkooN3x0cyQZHn7Xpb9jXGwgr7AuXZ0_E8sWE0d2lUSucud_6srI_55RUGx3dihONXNnSPsFXOu-ZokBsXI-vJ8aA8MTb3NlGDC1BkQtG3wz_aaxcIoNM0PzPw_mSV2qnHDVXGBXA-Cqvkw0XDuUlqfIMU_JzKlxKcBaFsW5rOd4o5JUoPaMrd6AJwjq9xR0aBOsz1_GWWaSL1z3EqSlsTK44hlapMLQdyYwrRZHJeXX_Zw1QjqPL5nfCWdHWqgIZfgLWcMcCzvkIRW1u5hfP7ekD2ey2ODlcaSXWVd_UAbLdN0PD6rs60wBWEFWvRAleKgrqVY4Nj3x6-TzgQlth3SkBLMABCRxVDVhGAQVwd4-cC36FGGefgHq4zAgcUViSDRQ-UUZ5totL2InnZ4AWnF4kateWjVvQyaAhdSrvBalPMJVUrqxyxWqM8LsxRvvsW8-qkSCMxKetjnbX9zHkPfmRxFDj2d-IKgUAkdwPfKpZ160Dd7u37iDfgtTslApPJXpp6TVVI1Sr4LlGzUV5iaV6bJ_Vmh8v1e_yQIpXcNAYDg3JUqxyksKGY-w2LdBWov5LRkr6dqGK4Ct_5pk2Utb"; // Change this to any URL you want to automatically use
 
     // Function to handle the logic for processing the URL
     function processUrl(url) {
